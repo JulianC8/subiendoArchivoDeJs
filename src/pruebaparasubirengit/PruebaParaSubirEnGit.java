e1f3dcc68d1a4477ad5c82e9c501b1c82bb7d5df
JulianC8 <125327461+JulianC8@users.noreply.github.com>
2023-07-04 16:27:54 -0500

    probandoPat

--- src/pruebaparasubirengit/PruebaParaSubirEnGit.java
+++ src/pruebaparasubirengit/PruebaParaSubirEnGit.java
@@ -1,10 +1,15 @@
 
 package pruebaparasubirengit;
 
+import java.util.Scanner;
+
 public class PruebaParaSubirEnGit {
 
     public static void main(String[] args) {
         System.out.println("Esto es una prueba");
+        Scanner nombres = new Scanner (System.in);
+        System.out.println("Digite un nombre");
+        int nombre1 = nombres.nextInt();
     }
     
 }
