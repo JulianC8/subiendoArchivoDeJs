d996e9e0838efcb986dd1eb4e40d0d700c421991
JulianC8 <125327461+JulianC8@users.noreply.github.com>
2023-07-07 13:15:39 -0500

    avances

--- src/pruebaparasubirengit/PruebaParaSubirEnGit.java
+++ src/pruebaparasubirengit/PruebaParaSubirEnGit.java
@@ -1,23 +1,16 @@
 
-package pruebaparasubirengit;
-
 import java.util.Scanner;
+
+
 
 public class PruebaParaSubirEnGit {
 
     public static void main(String[] args) {
-        System.out.println("Esto es una prueba");
-        Scanner nombres = new Scanner (System.in);
-        System.out.println("Digite un nombre");
-        int nombre1 = nombres.nextInt();
-        Scanner apellidos = new Scanner (System.in);
-        System.out.println("Digite un apellido");
-        int apellido1 = apellidos.nextInt();
-        Scanner numeros = new Scanner (System.in);
-        System.out.println("Digite un numero");
-        int numero1 = numeros.nextInt();
-        System.out.println("Por favor digite un numero");
-        
+        Scanner prueba = new Scanner(System.in);
+        System.out.println("Como hacer una suma en JAVA");
+        int prueba1 = prueba.nextInt();
+        System.out.println("Digite tres numeros");
+        int prueba2 = prueba.nextInt();
     }
     
 }
