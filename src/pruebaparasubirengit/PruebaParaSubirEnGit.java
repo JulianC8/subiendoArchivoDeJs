3a8ca06c1991d64b2771532b1e2270d16235293f
JulianC8 <125327461+JulianC8@users.noreply.github.com>
2023-07-10 11:21:05 -0500

    avances

--- src/pruebaparasubirengit/PruebaParaSubirEnGit.java
+++ src/pruebaparasubirengit/PruebaParaSubirEnGit.java
@@ -6,7 +6,13 @@ public class PruebaParaSubirEnGit {
     public static void main(String[] args) {
         int numeroUno = 23;
         int numeroDos = 24;
+        int tablas ;
+        Scanner primeraTabla = new Scanner (System.in);
         System.out.println(numeroUno);
+        System.out.println("Tablas de multiplicar");
+        System.out.println("Por favor indique el numero de la tabla a consultar");
+        int tabla = numeroUno + numeroDos;
+        System.out.println("El resultado de " + numeroUno + " y " + numeroDos + "es "+ tabla);
 //        Scanner prueba = new Scanner(System.in);
         //System.out.println("Como hacer una suma en JAVA");
         //int prueba1 = prueba.nextInt();
